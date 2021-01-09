3c12d5df3fa5c4dbe74bfc9635d30acf3dd4b4f2
unknown <varachkin@gmail.com>
2021-01-09 20:30:31 +0300

    add calculator logic in JS

--- Calculator/script/script.js
+++ Calculator/script/script.js
@@ -1,3 +1,38 @@
+let output = document.getElementById("out-bottom");
+let memory = document.getElementById("out-top");
+let dataMemory;
+function insert(num) {
+    if(output.innerText.length <= 10){
+        if(output.innerText === '0'){
+            output.innerText = num;
+        }else {
+            output.innerText += num;
+        }
+    }
+    if(num === '/' || num === '*' || num === '+' || num === '-'){
+        memory.innerText += output.innerText;
+        output.innerText = '0';
+    }
+}
+function clean() {
+    output.innerText = '0';
+    memory.innerText = '';
+}
+function backspace() {
+    let num = output.innerText;
+    if(num.length < 1){
+        output.innerText = '0';
+    }else {
+        output.innerText = num.substring(0, num.length - 1);
+    }
+}
+function result() {
 
-let int = document.getElementById("int");
-let int = true;
+}
+function memoryWrite() {
+    dataMemory = output.innerText;
+
+}
+function memoryRead() {
+    output.innerText = dataMemory;
+}
