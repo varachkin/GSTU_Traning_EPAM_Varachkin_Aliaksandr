c06586926f78a69b9806bab3f9869a60494c0bb9
unknown <varachkin@gmail.com>
2021-01-30 19:18:40 +0300

    correct bad

--- Calculator/script/script.js
+++ Calculator/script/script.js
@@ -6,13 +6,8 @@ let d = document.getElementById('_dot');
 let res = document.getElementById('_result')
 let pr = document.getElementById('priority');
 let dataMemory;
-let arrResult = [];
-let arr = [];
-let arrFin = [];
-let flag;
 
 function dot(num){
-    output.style.fontSize = '71px';
     if (num === '1'){
         res.classList.add('btn_big');
         d.disabled = true;
@@ -22,24 +17,18 @@ function dot(num){
     }
 }
 function insert(num) {
-    output.style.fontSize = '71px';
-    // Проверка строки на наличие '='. Если есть такой символ, после ввода любого символа поле очищается.
     for(let i = 0; i < memory.innerText.length; i++){
         if(memory.innerText[i] === '='){
-
-            if(num === '.'){
-                output.innerHTML = '0' + num;
-                memory.innerHTML = '0' + num;
-            }else{
-                output.innerHTML += num;
-                memory.innerHTML += num;
-            }
+            memory.innerHTML = '';
+            output.innerHTML = '';
+            output.innerHTML += num;
+            memory.innerHTML += num;
             return;
         }
     }
-    if(output.innerHTML === '0' && num === '.' ){
-        memory.innerHTML = '0' + num;
+    if(output.innerHTML === '0' && num === '.' && output.innerHTML[output.innerHTML.length - 1] === undefined){
         output.innerHTML += num;
+        memory.innerHTML = output.innerHTML;
         return;
     }
     if(output.innerHTML.length <= 10){
@@ -55,42 +44,24 @@ function insert(num) {
             output.innerHTML += num;
         }
     }
-    if(num === '.'){
-        for(let i = 0; i < output.innerText.length; i++){
-            if(output.innerText[i] === '.'){
-                return;
-            }
-        }
-    }
+
 }
 
 function act(symbol){
-    output.style.fontSize = '71px';
     let x = symbol;
-    if(flag){
-        memory.innerText = output.innerText + symbol;
-        console.log('flag' + flag);
-        output.innerText = '';
-        flag = false;
-    }
-    if(symbol === '/' || symbol === '*' || symbol === '-' || symbol === '+'){
-        if(output.innerText[output.innerText.length - 1] === '.'){
-            return;
-            }
-        }
     if(output.innerHTML === '0'){
         memory.innerHTML += output.innerText + symbol;
-        arrFin.push(output.innerText, symbol);
         output.innerText = '';
         return;
     }
     if(memory.innerText[memory.innerText.length - 1] === '/' || memory.innerText[memory.innerText.length - 1] === '*' || memory.innerText[memory.innerText.length - 1] === '+' || memory.innerText[memory.innerText.length - 1] === '-') {
         let num = memory.innerText;
-        arrFin.push(output.innerText, symbol);
         memory.innerHTML = num.substring(0, num.length - 1);
         memory.innerText += x;
+        console.log(memory.innerText[memory.innerText.length - 1]);
+        console.log(memory.innerText[memory.innerText.length - 2]);
+        console.log(memory.innerText[memory.innerText.length - 3]);
     }else {
-        arrFin.push(output.innerText, symbol);
         memory.innerText += symbol;
         output.innerText = '';
     }
@@ -99,23 +70,20 @@ function act(symbol){
 function clean() {
     output.innerHTML = '0';
     memory.innerHTML = '';
-    output.style.fontSize = '71px';
 }
 
 function backspace() {
     let num = output.innerHTML;
     let memoryNum = memory.innerHTML;
-    if(num.length === 1 && num === '0' && memoryNum.length === 1){
+    if(num.length === 1 && memoryNum.length === 1){
         output.innerHTML = '0';
         memory.innerHTML = '';
-
+        console.log('if');
     }else {
-        output.innerHTML = output.innerHTML.slice(0, -1);
-        if(memory.innerText[memory.innerText.length - 1] === '/' || memory.innerText[memory.innerText.length - 1] === '*' || memory.innerText[memory.innerText.length - 1] === '+' || memory.innerText[memory.innerText.length - 1] === '-'){
-
-        }else{
-            memory.innerText = memory.innerText.slice(0, -1);
-        }
+        // Проверить
+        output.innerHTML = num.substring(0, num.length - 1);
+        memory.innerText = memoryNum.substring(0, num.length - 1);
+        console.log('else');
     }
 }
 
@@ -128,7 +96,12 @@ function result(symbol) {
     }
 
     let mem = memory.innerText;
-    arrFin.push(output.innerText);
+    console.log(mem.length);
+    console.log(mem);
+    console.log(mem[mem.length - 1]);
+    console.log(mem[mem.length - 2]);
+    console.log(mem[mem.length - 3]);
+
     if(pr.checked){
         let x;
         let y;
@@ -136,104 +109,30 @@ function result(symbol) {
         y = Math.round(x);
         if(integer.checked){
             output.innerText = y;
-            arrResult.push(output.innerText);
-            console.log(arrResult);
             memory.innerText += symbol + y;
-            arr.push(memory.innerText);
-            console.log(arr);
             history.innerHTML += '<span style="margin-right: 20px">integer: </span>' +  memory.innerText + '<hr>';
         }else{
             x = +x.toFixed(9);
             x = x.toString();
             output.innerText = x;
-            arrResult.push(output.innerText);
-            console.log(arrResult);
             memory.innerText += symbol + x;
-            arr.push(memory.innerText);
-            console.log(arr);
             history.innerHTML += '<span style="margin-right: 20px">float: </span>' +  memory.innerText + '<hr>';
         }
     }else{
-        let n;
-        let res;
-        for(let i = 0; i < arrFin.length; i = i + 2){
-            if(i === 0){
-                n = +arrFin[i];
-            }
-            let f = String(arrFin[i+1]);
-            let m = +arrFin[i+2];
-
-            if(f === '/'){
-                res = (+n / m);
-                console.log(n);
-                console.log(f);
-                console.log(m);
-                console.log('result = ' + res);
-
-            }
-            if(f === '*'){
-                res = (+n * m);
-                console.log(n);
-                console.log(f);
-                console.log(m);
-                console.log('result = ' + res);
-
-            }
-            if(f === '-'){
-                res = (+n - m);
-                console.log(n);
-                console.log(f);
-                console.log(m);
-                console.log('result = ' + res);
-
-            }
-            if(f === '+'){
-                res = (+n + m);
-                console.log(n);
-                console.log(f);
-                console.log(m);
-                console.log('result = ' + res);
-
-            }
-            n = res;
-            console.log('N = RES = ' + n);
-            console.log('typeof n = ' + typeof n);
-        }
-        output.innerText = n;
-        arrResult.push(output.innerText);
-        console.log(arrResult);
-        memory.innerText += symbol + n;
-        arr.push(memory.innerText);
-        console.log(arr);
-        if(integer.checked){
-            history.innerHTML += '<span style="margin-right: 20px">integer - priority off: </span>' +  memory.innerText + '<hr>';
-        }else{
-            history.innerHTML += '<span style="margin-right: 20px">float - priority off: </span>' +  memory.innerText + '<hr>';
-        }
+        // Проверить
+        // let x = mem[mem.length[0]];
+        // for(let i = 0; i <= mem.length; i+3){
+        //     x = x + mem[mem.length[i + 1]] + mem[mem.length[i + 2]];
+        //     x = eval(x);
+        // }
     }
-    let temp = output.innerText;
-    if(temp.length > 11){
-        output.style.fontSize = '35px';
-    }
-    flag = arrResult;
-    console.log(arrResult)
-    console.log(flag);
-    arrFin = [];
 }
 function memoryWrite() {
-    dataMemory = +output.innerHTML;
-    if(dataMemory < 0){
-        dataMemory = '(' + String(dataMemory) + ')';
-    }else{
-        dataMemory = String(dataMemory);
-    }
+    dataMemory = output.innerHTML;
 }
 
 function memoryRead() {
     if(dataMemory){
-        if(dataMemory.length > 11){
-            output.style.fontSize = '35px';
-        }
         let x = memory.innerText;
         if(x.length >= 2){
             output.innerHTML += dataMemory;
@@ -245,4 +144,5 @@ function memoryRead() {
     }else{
         alert('no stored values');
     }
+
 }
